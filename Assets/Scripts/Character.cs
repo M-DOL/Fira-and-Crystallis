a50317b6641c04bfbd34f90501137c5888a9d4d1
Brandon Kittle <brakittl@umich.edu>
2016-02-05 20:20:01 -0500

    Crap Fire Projectile

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -23,4 +23,7 @@ public class Character : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	}
+
+    public virtual void Attack() {
+    }
 }
--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -3,12 +3,11 @@ using System.Collections;
 
 public class Fire : Character
 {
-
     // Use this for initialization
     void Start()
     {
-
         rb = GetComponent<Rigidbody2D>();
+        attacking = false;
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -23,8 +22,36 @@ public class Fire : Character
 
 
     // Update is called once per frame
+    public GameObject fireBlast, fireBlastGO;
+    public float blastTime = 3f;
+    public bool attacking = false;
+    float blastStart;
     void Update()
     {
         Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //Start Attack
+        if (Input.GetKey(KeyCode.Space) && !attacking) {
+            Attack();
+            blastStart = Time.time;
+            attacking = true;
+        }
+
+        //End Attack
+        if (attacking && (Time.time - blastStart > blastTime)) {
+            Destroy(fireBlastGO);
+            attacking = false;
+        }
+    }
+
+
+    public override void Attack() {
+        //See what direction it is facing
+        //Create fireBlast in that direction
+        //      Make fireBlast script that reacts if in contact w/ something
+        Vector3 pos = transform.position;
+        pos.y++;
+        print(transform.forward);
+        fireBlastGO = Instantiate(fireBlast, pos, Quaternion.identity) as GameObject;
+
     }
 }
