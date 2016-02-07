a1e5640200efeb3d808468769aa0e08900b10489
FriedRice <n.mclaughlin21@gmail.com>
2016-02-06 23:28:20 -0500

    Added another level with hot and cold water

---
+++ Assets/Scripts/WaterTile.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterTile : MonoBehaviour {
+    public Color neutral, water;
+    public enum WaterType { Hot, Cold };
+
+    public float water_removed_time = 1f;
+    public bool water_removed;
+    public WaterType water_type;
+
+    SpriteRenderer sr;
+    Coroutine stopping;
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            if ((collision.gameObject.name == "Fire" && water_type == WaterType.Hot) ||
+                    collision.gameObject.name == "Ice" && water_type == WaterType.Cold) {
+                water_removed = true;
+                sr.color = neutral;
+                if (stopping != null) {
+                    StopCoroutine(stopping);
+                }
+            }
+            else if (!water_removed) {
+                if (water_type == WaterType.Hot) {
+                    Ice.S.Kill();
+                } else if (water_type == WaterType.Cold) {
+                    Fire.S.Kill();
+                }
+            }
+        }
+    }
+
+    IEnumerator cooldown(float f) {
+        yield return new WaitForSeconds(f);
+        sr.color = water;
+    }
+
+    public void OnTriggerExit2D(Collider2D collision) {
+        stopping = StartCoroutine(cooldown(water_removed_time));
+    }
+
+    // Use this for initialization
+    void Start() {
+        sr = GetComponent<SpriteRenderer>();
+        if (this.gameObject.tag == "HotWater") {
+            water_type = WaterType.Hot;
+        } else if (this.gameObject.tag == "ColdWater") {
+            water_type = WaterType.Cold;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
