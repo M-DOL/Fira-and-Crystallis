3eacd4d9355bb595d7ce72b68c1c22e7b6364ffe
M-DOL <mjdolan@umich.edu>
2016-02-07 17:00:27 -0500

    Bridge Death

--- Assets/Scripts/Bridge.cs
+++ Assets/Scripts/Bridge.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Bridge : MonoBehaviour
 {
     public GameObject Disables;
-
+    bool fireOn = false, iceOn = false;
     public void Activate()
     {
         Disables.SetActive(false);
@@ -13,12 +13,38 @@ public class Bridge : MonoBehaviour
 
     public void Deactivate()
     {
+        if(fireOn)
+        {
+            Level.S.KillCharacter(Fire.S.gameObject);
+        }
+        if(iceOn)
+        {
+            Level.S.KillCharacter(Ice.S.gameObject);
+        }
         Disables.SetActive(true);
         GetComponent<SpriteRenderer>().enabled = false;
     }
-
-    void Start()
+    void OnTriggerEnter2D(Collider2D collision)
     {
+        if(collision.gameObject.tag == "Fire")
+        {
+            fireOn = true;
+        }
+        if (collision.gameObject.tag == "Ice")
+        {
+            iceOn = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Fire")
+        {
+            fireOn = false;
+        }
+        if (collision.gameObject.tag == "Ice")
+        {
+            iceOn = false;
+        }
     }
 
     // Update is called once per frame
