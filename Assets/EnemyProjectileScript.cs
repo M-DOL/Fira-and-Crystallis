5edad94bd9d9eef797a4cfd9d70be8d85f0175f3
charblaze <stalistalev@hotmail.com>
2016-02-07 20:13:12 -0500

    Added level 2-2

---
+++ Assets/EnemyProjectileScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyProjectileScript : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
