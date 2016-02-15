141215671c8f9c737f4fec1d34a00710de9b5491
Stali Stalev <Stali Stalev>
2016-02-15 16:00:47 -0500

    new boss battle abilities

---
+++ Assets/laser.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class laser : MonoBehaviour
+{
+    bool lethal = true;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!lethal)
+        {
+            return;
+        }
+         if(collision.tag == "Player")
+        {
+            if(collision.gameObject.name == "Fire")
+            {
+                Fire.S.Kill();
+            } else
+            {
+                Ice.S.Kill();
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(animate());
+    }
+
+    IEnumerator animate()
+    {
+        yield return new WaitForSeconds(0.3f);
+        lethal = false;
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject.transform.parent.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
